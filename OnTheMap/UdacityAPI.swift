d4f716ab8854d00d7de4061e17c12d8234b5e1d1
Asbar <s201449260@kfupm.edu.sa>
2019-06-03 04:16:38 +0300

    sss

--- OnTheMap/UdacityAPI.swift
+++ OnTheMap/UdacityAPI.swift
@@ -78,7 +78,7 @@ class UdacityAPI {
             var request = URLRequest(url: URL(string: "https://onthemap-api.udacity.com/v1/StudentLocation")!)
             request.httpMethod = "POST"
             request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.httpBody = "{\"uniqueKey\": \"1234\", \"firstName\": \"John\", \"lastName\": \"Doe\",\"mapString\": \"Mountain View, CA\", \"mediaURL\": \"https://udacity.com\",\"latitude\": 37.386052, \"longitude\": -122.083851}".data(using: .utf8)
+            request.httpBody = "{\"uniqueKey\": \"1234\", \"firstName\": \"John\", \"lastName\": \"Doe\",\"mapString\": \"\(locationName)\", \"mediaURL\": \"\(link)\",\"latitude\": \"\(locationCoordinate.latitude)\" , \"longitude\": \"\(locationCoordinate.longitude)\"}".data(using: .utf8)
             let session = URLSession.shared
             let task = session.dataTask(with: request) { data, response, error in
                 if error != nil { // Handle error…
